6fab48b1b82e51e6d978905dd10d8d7e6ddc0f94
Krupa Bathia <krupa.bathia@gmail.com>
2015-04-28 21:07:54 +0530

    Formatting the chart, making it more attractive

--- src/vcdview.py
+++ src/vcdview.py
@@ -292,18 +292,18 @@ class vcd_reader:
                             bus=1
                         else:
                             bus=0
-                        json_string='{"name":"'+name+'","showInLegend": "true",'
+                        json_string='{"name":"'+name+'","showInLegend": "true","toolTipContent":"'+name+',{x}","markerType": "none",'
 			json_string=json_string+'"type":"line","dataPoints":['
 			symbol=self.signal_symbol_dict[name]
 			x_y_pairs=[]
 			for i in range(len(self.transitions_dict[symbol][0])-1):
 				#print self.transitions_dict[symbol][1][i]
 			     if bus==0:
-				x_y_pairs.append((self.transitions_dict[symbol][0][i],str(int(self.transitions_dict[symbol][1][i])+voltage_val)))
-				x_y_pairs.append((self.transitions_dict[symbol][0][i+1],str(int(self.transitions_dict[symbol][1][i])+voltage_val)))
+					x_y_pairs.append((self.transitions_dict[symbol][0][i],str(int(self.transitions_dict[symbol][1][i])+voltage_val)))
+					x_y_pairs.append((self.transitions_dict[symbol][0][i+1],str(int(self.transitions_dict[symbol][1][i])+voltage_val)))
 			     else:
-				x_y_pairs.append((self.transitions_dict[symbol][0][i],str(int(self.transitions_dict[symbol][1][i]))))
-				x_y_pairs.append((self.transitions_dict[symbol][0][i+1],str(int(self.transitions_dict[symbol][1][i]))))
+					x_y_pairs.append((self.transitions_dict[symbol][0][i],str(int(self.transitions_dict[symbol][1][i]))))
+					x_y_pairs.append((self.transitions_dict[symbol][0][i+1],str(int(self.transitions_dict[symbol][1][i]))))
 			if bus==0:
 			     x_y_pairs.append((self.transitions_dict[symbol][0][len(self.transitions_dict[symbol][0])-1],str(int(self.transitions_dict[symbol][1][len(self.transitions_dict[symbol][0])-1])+voltage_val)))
 			     x_y_pairs.append((self.end_time,str(int(self.transitions_dict[symbol][1][len(self.transitions_dict[symbol][0])-1])+voltage_val)))
@@ -338,7 +338,7 @@ class vcd_reader:
                                         bus_bit=1-bus_bit
                                 else:
                                         bus_val=''
-				json_string=json_string+'{"x":'+str(time/1000000)+',"y":'+str(bus_bit+voltage_val)+',"indexLabel": "'+bus_val+'","indexLabelLineThickness":1,"indexLabelMaxWidth": 1, "indexLabelPlacement": "inside"}'
+				json_string=json_string+'{"x":'+str(time/1000000)+',"y":'+str(bus_bit+voltage_val)+',"indexLabel": "'+bus_val+'","indexLabelLineThickness":1,"indexLabelMaxWidth": 1, "indexLabelPlacement": "inside","indexLabelFontSize": 14,"indexLabelFontColor": "black"}'
                                 
                                 previous_val=val
                                     
@@ -349,7 +349,7 @@ class vcd_reader:
 
 			json_string=json_string+']}'
 			#print json_string
-			voltage_val=voltage_val+1.25
+			voltage_val=voltage_val+2
 			dict_to_return[name]=json_string
 	return(dict_to_return)
 
